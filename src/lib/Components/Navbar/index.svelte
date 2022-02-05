8869ab3ef64703e98ce6418dd8bdf42c1ff04310
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-02-05 11:25:05 +0000

    The toggler for the navbar hides on smaller screens

--- src/lib/Components/Navbar/index.svelte
+++ src/lib/Components/Navbar/index.svelte
@@ -4,5 +4,6 @@
 
 <nav class="fixed flex bg-blue-700 top-0 w-full p-3">
     <a href="/" class="italic text-white font-bold">Many Toolz</a>
-    <button class="ml-auto bg-transparent p-0 hover:bg-transparent active:bg-transparent text-white"><Menu/></button>
+    <button class="md:hidden bg-transparent p-0 hover:bg-transparent active:bg-transparent ml-auto text-white"><Menu/></button>
+    
 </nav>
